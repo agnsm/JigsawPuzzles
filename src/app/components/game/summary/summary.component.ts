ecc5713e75014eda142518f63ae8b4a0e1544bb0
Agnieszka <agnieszka.smolinska.99@gmail.com>
2022-10-05 14:33:25 +0200

    style summary component

--- src/app/components/game/summary/summary.component.ts
+++ src/app/components/game/summary/summary.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { faPuzzlePiece } from '@fortawesome/free-solid-svg-icons';
 import { Subscription } from 'rxjs';
+import { fadeEnterAnimation } from 'src/app/helpers/animation';
 import { GameProgress } from 'src/app/models/interfaces/game-progress';
 import { GameService } from 'src/app/services/game.service';
 
 @Component({
   selector: 'app-summary',
+  animations: [fadeEnterAnimation],
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.scss']
 })
 export class SummaryComponent implements OnInit, OnDestroy {
   gameProgress!: GameProgress;
   gameProgressSubscription!: Subscription;
+
+  faPuzzlePiece = faPuzzlePiece;
 
   constructor(private gameService: GameService) { }
 
